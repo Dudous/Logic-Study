d5998a6e6d74cb781743778f942500a287bc2906
Dudous <sampaioeeduardo36@gmail.com>
2024-03-11 17:09:39 -0300

    Hello World em C

---
+++ HelloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 10;
+    // float flt = 10.5;
+    // char char1 = 'a';
+    char nome[8] = "Nicolas";
+
+    printf("%s %d", nome, num1);
+
+    return 0;
+}

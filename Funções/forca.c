7588e5b741202f94526da986a524398ec8ebc030
Dudous <sampaioeeduardo36@gmail.com>
2024-03-26 17:17:10 -0300

    Bibliotecas

--- Funções/forca.c
+++ Funções/forca.c
@@ -3,25 +3,74 @@
 #include <string.h>
 #include <time.h>
 
-#define Tentativas 6
+#define Tentativas 7
 
 void Boneco(int tentativas){
-    char boneco[7][20] = {" O \n", "/", "|", "\\\n", "/ ", "\\"};
+    char boneco[7][20] = {"|   O ", "\n|   |", "\b\b/", "|\\ \n|", "\b|  / ", "\\"};
 
-    for(int i = 0; i <= tentativas - 1; i++){
-        printf("%s", boneco[i]);
+    printf("\n\n ____\n");
+    printf("|   |\n");
+
+    if(tentativas ==0)
+        printf("|");
+    else{
+        for(int i = 0; i <= tentativas - 1; i++){
+            printf("%s", boneco[i]);
+        }
+    }
+    printf("\n|   \n|__________________________________\n");
+}
+
+
+char *Palavra() {
+    char *words[5] = {"boneco", "barata", "molejo", "tucano"};
+    srand(time(NULL));
+    return strdup(words[rand() % 4]);
+}
+
+void Palpite(char palpite[7]){
+    for(int i = 0;i <= 6;i++){
+        printf("%c ", palpite[i]);
     }
 }
 
-char Palavra(){
-    char words[5][7] = {"sla","dejo"};
-    srand(time(NULL));
-    char *word = words[(rand() %8)];
+int main() {
+    char letter, palpite[7] = "______";
+    int correct, erros = 0;
 
-}
+    char *palavra = Palavra();
+    // for(int j= 0;j<= 7;j++){
+    //         printf("%c\n", palavra[j]);
+    // }
 
-int main(){
-    Boneco(1);
+    printf("%s\n\n", palavra);
+    
 
+    for (int i = 0; i < Tentativas; i++)
+    {
+        Boneco(erros);
 
+        printf("\n\n");
+
+        Palpite(palpite);
+
+        printf("\n\nDigite uma Letra:");
+        scanf("%s", &letter);
+        
+        correct = 0;
+
+        for(int j= 0;j<= 6;j++){
+            printf("%c\n", palavra[j]);
+            if(letter == palavra[j])
+            {
+                palpite[j] = letter;
+                correct = 1;
+            }
+        }
+
+        if (correct == 0){
+            erros++;
+        }
+    }
+    
 }
---
+++ banco.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#ifndef BANCO_H
+#define BANCO_H
+
+
+
+void Deposit(float value){
+    printf("Deposito efetuado no valor de: %f", value);
+}
+
+
+
+#endif

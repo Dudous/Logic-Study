0d2f69e6ffec2c05328b78f3c09a6975418785c5
Dudous <sampaioeeduardo36@gmail.com>
2024-04-18 16:53:05 -0300

    malloc

--- Array/velha.c
+++ Array/velha.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
+#include "velha.h"
 
 int main()
 {
     int user, cont = 1;
-    char jogador, jogo[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+    char jogador, jogo[3][3] = {{'.','.','.'},{'.','.','.'},{'.','.','.'}};
     
-    while (1)
+    while (Vencedor(jogo))
     {
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                printf("\t %d", jogo[i][j]);
+                printf("\t %c", jogo[i][j]);
             }
             printf("\n\n");
         }
-        printf("Digite a posição, Jogador %d\n", cont);
+        printf("Digite a posicao, Jogador %d\n", (cont%2 == 1)? 1: 2);
         scanf(" %d", &user);
 
         user --;
 
         jogador = (cont%2 == 0)? 'O': 'X';
 
-        jogo[user/3][user%3] = jogador;
-
-        cont++;
+        if(jogo[user/3][user%3] == '.')
+        {
+            jogo[user/3][user%3] = jogador;
+            cont ++;
+        }
+        else{
+            printf("Posição inválida, tente novamente!!\n\n");
+        } 
     }
     
 

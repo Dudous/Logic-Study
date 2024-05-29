74ea47bf9e12b77cce7b1361d2b1138a3d2bb8d0
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-05-29 16:21:44 -0300

    Linked List

---
+++ LinkedList/LinkedList.h
@@ -0,0 +1,87 @@
+#ifndef LinkedList_H
+#define LinkedList_H
+
+typedef struct NodeList
+{
+    int value;
+    struct NodeList *next;
+}NodeList;
+
+typedef struct LinkedList
+{
+    NodeList *head;
+    int size;
+}LinkedList;
+
+
+LinkedList LinkedListConstructor()
+{
+    LinkedList new_linked;
+
+    new_linked.head = NULL;
+    new_linked.size = 0;
+    return new_linked;
+}
+
+
+void addlist(LinkedList list, int value)
+{
+    list.size++;
+    NodeList node;
+    node.value = value;
+
+    if(!list.head)
+    {
+        list.head = node;
+        return;
+    }
+
+    NodeList iterator = list.head;
+    while(!iterator.next)
+    {
+        iterator = iterator.next;
+    }
+
+    iterator.next = node;
+
+    return;
+}
+
+// int removeList(LinkedList list, int index)
+// {
+//     if(index >= size)
+//         return NULL;
+    
+//     size--;
+
+//     NodeList iterator = list.head;
+
+//     for (int i = 1; i < index; i++)
+//     {
+//         iterator = iterator.next;
+//     }
+    
+//     NodeList aux = iterator;
+//     aux = iterator.next;
+
+//     iterator.next = aux.next;
+
+//     return aux.value;
+// }
+
+// int getList(LinkedList list, int index)
+// {
+//     if(index >= size)
+//         return NULL;
+    
+//     NodeList iterator = list.head;
+
+//     for (int i = 1; i <= index; i++)
+//     {
+//         iterator = iterator.next;
+//     }
+
+//     return iterator.value;
+// }
+
+#endif // !LinkedList
---
+++ LinkedList/Pilha.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+
+void push(LinkedList * list, int value)
+{
+    add(list, value);
+}
+
+// int pop(LinkedList * list)
+// {
+//     return removeList(list, list->size -1);
+// }
+
+// int peek(LinkedList * list)
+// {
+//     return getList(list, list->size -1);
+// }
+
+
+int main()
+{
+    LinkedList list = LinkedListConstructor();
+
+    push(list, 1);
+    push(list, 2);
+    push(list, 3);
+    push(list, 4);
+
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     printf("Pop %d = %d", i, pop(list));
+    // }
+
+}

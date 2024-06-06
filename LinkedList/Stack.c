5afca4856b4cd2355c05df83aede4e5d2d0fdc28
Dudous <sampaioeeduardo36@gmail.com>
2024-06-06 16:41:57 -0300

    Stack concluded

---
+++ LinkedList/Stack.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+
+void push(LinkedList * list, int value)
+{
+    addlist(list, value);
+}
+
+int pop(LinkedList * list)
+{
+    return removeList(list, list->size -1);
+}
+
+int peek(LinkedList * list)
+{
+    return getList(list, list->size -1);
+}
+
+
+int main()
+{
+    LinkedList * list = LinkedListConstructor();
+
+    push(list, 1);
+    push(list, 2);
+    push(list, 3);
+    push(list, 4);
+
+
+    printf("\nGET %d",peek(list));
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("\nPop %d",pop(list));
+    }
+
+}

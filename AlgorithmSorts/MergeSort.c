aa977a9078e04f64dd20401cf6aa22b622011831
Dudous <sampaioeeduardo36@gmail.com>
2024-06-11 17:07:51 -0300

    peguei a visão

--- AlgorithmSorts/MergeSort.c
+++ AlgorithmSorts/MergeSort.c
@@ -3,22 +3,56 @@
 
 void MergeSort(int * array, int start, int end)
 {
-    printf("\n\nMERGE 1\nstart %d , end %d", start,end);
     char a;
-    scanf(" %c",&a);
+
     if(start >= end)
         return;
 
-    MergeSort(array, start, end / 2 );
+    int mid = start + (end - start) / 2;
 
-    if(array[start] > array[end])
+    MergeSort(array, start, mid);
+
+    MergeSort(array, mid + 1 , end);
+
+    scanf(" %c", &a);
+
+    int * copy = malloc((end - start)* sizeof(int));
+
+    int second = mid + 1;
+
+    int pace = 0;
+
+    printf("\n\nMERGE \nstart %d , end %d, second %d , pace %d", start, end, second, pace);
+
+    while (pace <= end - start)
     {
-        array[start] = array[start] ^ array[end];
-        array[end] = array[start] ^ array[end];
-        array[start] = array[start] ^ array[end];
+        if(array[start] < array[second])
+        {
+            copy[start] = array[start];
+            copy[second] = array[second];
+            start++;
+        }
+        else
+        {
+            copy[start] = array[second];
+            copy[second] = array[start];
+            second++;
+        }
+
+        for (int i = start; i <= end; i++)
+        {
+            array[i] = copy[i];
+            printf("\t%d", array[i]);
+        }
+        
+        pace++;
     }
-    printf("\n\nMERGE 2\nstart %d , end %d", start,end);
-    MergeSort(array, (end / 2) + (end & 1), end);
+    
+    printf("\n\n");
+    for (int i = 0; i < 7; i++)
+    {
+        printf("\t%d", array[i]);
+    }
 
 }
 
@@ -28,6 +62,8 @@ int main()
     int array[7] = {3,1,2,5,9,7,6};
 
     MergeSort(array, 0, 6);
+
+    printf("\n");
 
     for (int i = 0; i < 7; i++)
     {

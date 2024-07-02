c619919d1bfbc838710742b87d4b9d8cbca00c2f
Dudous <sampaioeedaurdo36@gmail.com>
2024-07-02 19:47:17 -0300

    Agenda Telefonica incompleta

--- AgendaTelefonica/AgendaTelefonica.c
+++ AgendaTelefonica/AgendaTelefonica.c
@@ -1,46 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-typedef struct 
-{
-    char ddd;
-    char telefone[11];
-} Telefone;
-
-typedef struct 
-{
-    char rua[50];
-    int numero;
-    char complemento[50];
-    char bairro[30];
-    char cep[11];
-    char cidade[50];
-    char estado[50];
-    char pais[50];
-} Endereco;
-
-typedef struct 
-{
-    int dia;
-    int mes;
-    int ano;
-} Data;
-
-typedef struct 
-{
-    char nome[50];
-    char email[50];
-    Endereco endereco;
-    Telefone telefone;
-    Data nascimento;
-    char obs[100];
-} Pessoa;
+#include "Agenda.h"
 
 void removePessoa(Pessoa *list, int index)
 {
-    // free(list[index]);
-
     for (int i = index; i < 5; i++)
     {
         list[i] = list[i+1];
